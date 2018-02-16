2ad92cdf46c5188d51577bec029654e12fa8b60c
Batou <pierpaolo.busacchi@gmail.com>
2018-02-16 17:55:04 +0100

    added dfa for Matricola with tests, all working correctly

---
+++ src/DFA/Matricola.java
@@ -0,0 +1,59 @@
+package DFA;
+
+/*
+ * This particular dfa must accept strings formed by a string of number (of any dimension) followed by a string of letters (of any dimension) based on:
+ * if the string of number is even the string of letters MUST start with a-kA-k
+ * if is odd the string of letters MUST start with l-zL-z
+ */
+
+public class Matricola {
+    public static boolean scan(String s) {
+        int state = 0;
+        int i = 0;
+
+        while (state >= 0 && i < s.length()) {
+            final char ch = s.charAt(i++);
+
+            switch (state) {
+                case 0:
+                    if (!Character.isLetter(ch) && Character.getNumericValue(ch) % 2 == 0) //even
+                        state = 1;
+                    else if (!Character.isLetter(ch) && Character.getNumericValue(ch) % 2 != 0) //odd
+                        state = 2;
+                    else state = -1;
+                    break;
+
+                case 1:
+                    if (Character.toString(ch).matches("[a-kA-K]"))
+                        state = 3;
+                    else if (!Character.isLetter(ch) && Character.getNumericValue(ch) % 2 == 0) //even
+                        state = 1;
+                    else if (!Character.isLetter(ch) && Character.getNumericValue(ch) % 2 != 0) //odd
+                        state = 2;
+                    else state = -1;
+                    break;
+
+                case 2:
+                    if (Character.toString(ch).matches("[l-zL-Z]"))
+                        state = 3;
+                    else if (!Character.isLetter(ch) && Character.getNumericValue(ch) % 2 == 0) //even
+                        state = 1;
+                    else if (!Character.isLetter(ch) && Character.getNumericValue(ch) % 2 != 0) //odd
+                        state = 2;
+                    else state = -1;
+                    break;
+
+                case 3:
+                    if(Character.toString(ch).matches("[a-zA-Z]"))
+                        state = 3;
+                    else
+                        state = -1;
+            }
+        }
+        return (state == 3);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(NotThreeZeros.scan(args[0]) ? "Ok" : "Nope");
+    }
+}
---
+++ src/Testing/MatricolaTest.java
@@ -0,0 +1,47 @@
+package Testing;
+
+import DFA.Matricola;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MatricolaTest {
+
+    private List<TestCase> tc = new ArrayList<TestCase>();
+
+
+    private class TestCase {
+        String inputString;
+        Boolean shouldBeAccepted;
+
+        TestCase(String inputString, Boolean shouldBeAccepted) {
+            this.inputString = inputString;
+            this.shouldBeAccepted = shouldBeAccepted;
+        }
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        tc.add(new TestCase("123456Bianchi", true));
+        tc.add(new TestCase("654321Rossi", true));
+        tc.add(new TestCase("654321Bianchi", false));
+        tc.add(new TestCase("123456Rossi", false));
+        tc.add(new TestCase("2Bianchi", true));
+        tc.add(new TestCase("122C", true));
+        tc.add(new TestCase("Rossi", false));
+        tc.add(new TestCase("2341", false));
+        tc.add(new TestCase("2123132%Bianchi", false));
+    }
+
+
+    @Test
+    public void scan() {
+        for (TestCase elem : tc) {
+            String msg = "testing string: " + elem.inputString + " expecting: " + elem.shouldBeAccepted;
+            Assert.assertEquals(msg, elem.shouldBeAccepted, Matricola.scan(elem.inputString));
+        }
+    }
+}
