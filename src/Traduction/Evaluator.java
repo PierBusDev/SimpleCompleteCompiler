d42d74858e7244d418557dd0d1e988a572a60941
Batou <pierpaolo.busacchi@gmail.com>
2018-02-21 02:25:41 +0100

    First version of Evaluator seems working correctly!

--- src/Traduction/Evaluator.java
+++ src/Traduction/Evaluator.java
@@ -1,6 +1,7 @@
 package Traduction;
 
 import LexAnalysis.Lexers.Lexer;
+import LexAnalysis.NumberTok;
 import LexAnalysis.Tag;
 import LexAnalysis.Token;
 
@@ -37,52 +38,117 @@ public class Evaluator {
 
 
     public void start(){
-        int expr_val;
-        // TODO
-        expr_val = expr();
-        match(Tag.EOF);
+        int expr_val = 0;
+
+        // gui(start -> expr EOF) = { (, NUM }
+        if(look.tag == '(' || look.tag == Tag.NUM) {
+            expr_val = expr();
+            match(Tag.EOF);
+        }else{
+            error("syntax error: '" + look + "' is not in gui(start -> expr EOF)" );
+        }
 
         System.out.println(expr_val);
     }
 
-    private int expr(){
-        int term_val, exprp_val;
-        // TODO
-        term_val = term();
-        exprp_val = exprp(term_val);
-        // TODO
 
+    private int expr(){
+        int term_val, exprp_val = 0;
+
+        //gui(expr -> term exprp) = { (, NUM}
+        if(look.tag == '(' || look.tag == Tag.NUM){
+            term_val = term();
+            exprp_val = exprp(term_val);
+        }else{
+            error("syntax error: '" + look + "' is not in gui(expr -> term exprp)" );
+        }
         return exprp_val;
     }
 
     private int exprp(int exprp_i){
-        int term_val, exprp_val;
+        int term_val, exprp_val = 0;
 
-        switch (look.tag){
-            case '+':
-                match('+');
+        switch(look.tag){
+            case '+': //gui(exprp -> + term exprp) = +
+                move();
                 term_val = term();
                 exprp_val = exprp(exprp_i + term_val);
                 break;
-                //TODO
+            case '-': //gui(exprp -> - term exprp) = -
+                move();
+                term_val = term();
+                exprp_val = exprp(exprp_i - term_val);
+                break;
+            case ')': //gui(exprp -> epsilon) = { ), EOF}
+            case Tag.EOF:
+                //do nothing
+                exprp_val = exprp_i;
+                break;
+            default:
+                error("syntax error: '" + look + "' is not in gui(exprp -> ...)");
         }
+        return exprp_val;
     }
 
     private int term(){
+        int fact_val, term_val = 0;
 
+        if(look.tag == '(' || look.tag == Tag.NUM){ //gui(term -> fact termp) = { (, EOF}
+            fact_val = fact();
+            term_val = termp(fact_val);
+        }else{
+            error("syntax error: '" + look + "' is not in gui(term -> fact termp)" );
+        }
+
+        return term_val;
     }
 
     private int termp(int termp_i){
+        int fact_val, termp_val = 0;
+        switch(look.tag){
+            case '*': //gui(termp -> * fact termp) = *
+                move();
+                fact_val = fact();
+                termp_val = termp(termp_i * fact_val);
+                break;
+            case '/': //gui(termp -> / fact termp) = /
+                move();
+                fact_val = fact();
+                termp_val = termp(termp_i / fact_val);
+                break;
+            case '+': //gui(termp -> epsilon) = { +, -, ), EOF}
+            case '-':
+            case ')':
+            case Tag.EOF:
+                //do nothing
+                termp_val = termp_i;
+                break;
+            default:
+                error("syntax error: '" + look + "' is not in gui(termp -> ...)");
+        }
 
+        return termp_val;
     }
 
     private int fact(){
+        int fact_val = 0;
+        if(look.tag == '('){ // gui(fact ->( expr ) } = (
+            move();
+            fact_val = expr();
+            match(Token.rpt.tag);
+        }else if(look.tag == Tag.NUM){ //gui(fact -> NUM) = NUM
+            fact_val = Integer.valueOf( ((NumberTok) look).lexeme );
+            move();
+        }else{
+            error("syntax error: '" + look + "' is not in gui(fact-> ...)");
+        }
 
+        return fact_val;
     }
 
     public static void main(String[] args){
         Lexer lex = new Lexer();
-        String path = "src/Parsing/mathOperations.txt";
+        String path = "src/Traduction/mathOperations.txt";
         try{
             BufferedReader br = new BufferedReader(new FileReader(path));
             Evaluator parser = new Evaluator(lex, br);
